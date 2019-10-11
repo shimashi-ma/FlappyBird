017eaecd1da84fcafd3098a936dca76f66e859d7
mikako.kinugawa <kinugawa0807@yahoo.co.jp>
2019-10-11 20:25:31 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by mikako kinugawa on 2019/10/11.
+//  Copyright © 2019 mikako.kinugawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
